63d2f88a0c99815ed1d26c328744356a51dcb768
Dennis <denn054f@gmail.com>
2021-11-05 09:26:46 +0100

    update5

--- js/main.js
+++ js/main.js
@@ -1,87 +1,83 @@
+import { q } from './quiz1.js'
+
+// q = {
+//     name: 'test',
+//     created: new Date(),
+//     questions: [
+//         {
+//             category: 'continent',
+//             question: 'on which continent is Denmark located?1',
+//             possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+//             correctAnswer: 1
+//         },
+//         {
+//             category: 'continent',
+//             question: 'on which continent is Denmark located?2',
+//             possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+//             correctAnswer: 3
+//         },
+//         {
+//             category: 'continent',
+//             question: 'on which continent is Denmark located?3',
+//             possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+//             correctAnswer: 0
+//         },
+//         {
+//             category: 'continent',
+//             question: 'on which continent is Denmark located?4',
+//             possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+//             correctAnswer: 0
+//         }
+//     ]
+// }
+
 let score = 0;
-let q = {
-    name: 'test',
-    created: new Date(),
-    questions: [
-        {
-            category: 'continent',
-            question: 'on which continent is Denmark located?1',
-            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
-            correctAnswer: 1
-        },
-        {
-            category: 'continent',
-            question: 'on which continent is Denmark located?2',
-            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
-            correctAnswer: 3
-        },
-        {
-            category: 'continent',
-            question: 'on which continent is Denmark located?3',
-            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
-            correctAnswer: 0
-        },
-        {
-            category: 'continent',
-            question: 'on which continent is Denmark located?4',
-            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
-            correctAnswer: 0
-        }
-    ]
-}
 
-function init() {
-    const quizEl = document.getElementById('quiz')
-
-
-
+function init() {                                                 //creating everything
+    const quizElement = document.getElementById('quiz')
     let quizStr = ''
-    q.questions.forEach(function (questions, qIndex) {
+    q.questions.forEach(function (questions, qIndex) {            //looping through questions
         let answerStr = ''
         const answerOrder = randomRange(questions.possibleAnswers.length)
         questions.possibleAnswers.forEach(function (answer, aIndex) {
             answerStr += `
-      <li style="order: ${answerOrder[aIndex]}">
+      <li style="order: ${answerOrder[aIndex]}">                              
         <label>
-             <input type ="radio" name="question${qIndex}" data-correct="${questions.correctAnswer === aIndex}">     
+             <input type ="radio" name="question-${qIndex}" data-correct="${questions.correctAnswer === aIndex}">     
              ${answer} 
         </label>         
       </li>`
 
-
-
-        })
-
+        })    //creating a radio button for each answer including html elements... same name to radiobtn, to prevent all from being checked
 
         quizStr += `
     <form>
       <h1>${questions.question}</h1>
       <div class="alert"></div>
+      <div class="score">Score: ${score}</div>
       <ul style="display: flex; flex-direction: column;">
         ${answerStr}
       </ul>
       <button type="submit">Submit</button>
     </form>`
 
-
-
     })
-    quizEl.innerHTML = quizStr
-
-    quizEl.addEventListener('submit', function (e) {
-        e.preventDefault()
+    quizElement.innerHTML = quizStr
+    quizElement.addEventListener('submit', function (e) {
+        e.preventDefault()                                                           //preventing the page from reloading on submit
         const alert = e.target.querySelector('div.alert')
-        const selectedInput = e.target.querySelector('input[type=radio]:checked')
+        const selectedInput = e.target.querySelector('input:checked')
         if (selectedInput === null) {
             alert.innerHTML = 'Select an answer'
         } else if (selectedInput.dataset.correctAnswer === "true") { //bugged
             alert.innerHTML = 'Correct'
             score++
+            localStorage.setItem('score', score);
             console.log(score)
         } else {
             alert.innerHTML = 'Wrong'
 
-        }
+        } console.log(selectedInput)
     })
 
 }
@@ -89,19 +85,18 @@ function init() {
 //calling quiz, creating radio buttons etc.
 init()
 
-
 //shuffle
 function shuffle(n) {
-    const randomNum = Math.random() * n
+    const randomNum = Math.random() * n    //generating a random number
     return Math.floor(randomNum)
 }
 
 function randomRange(x) {
     const arr = []
-    for (let i = 0; i < x; i += 1) {
+    for (let i = 0; i < x; i += 1) {   //generating an array of numbers example randomRange(5) = [1]...[5]
         arr.push(i)
     }
-    const randomArr = []
+    const randomArr = []               //picking random item from arr, creating new array in random order
     while (arr.length > 0) {
         const randomIndex = shuffle(arr.length)
         const randomNumber = arr[randomIndex]
@@ -112,36 +107,3 @@ function randomRange(x) {
     return randomArr
 
 }
-
-
-
-
-
-
-
-// Kravet er, at I skal lave et site med mindst to quizzer.
-// JavaScripten skal struktureres med moduler, så
-// HTML - siden kun refererer én JavaScript - fil.
-// De to quizzer skal strukturelt være ens, så den
-// JavaScript - kode, der viser quizzerne skal være ens. 
-// Det betyder, at der skal skrives en funktion der 
-// modtager en quiz som inputparameter, hvorefter den
-// viser quizzen.Fx
-
-// const displayQuiz = function (quiz, where) { };
-
-// Funktionen skal kalde quizzens toString metode.Den 
-// skal selv kunne kaldes således:
-
-// displayQuiz(quiz1, div1);
-// displayQuiz(quiz2, div2);
-// displayQuiz(quizn, divn);
-
-// Hvor quiz1 - quizn er konkrete eksempler på den 
-// ovenfor viste datastruktur, og div1 - divn er id’er 
-// på de dom - elementer på siden, hvor quizzen skal stå.
-// Når en quiz vises, skal metoden shuffle() sørge for,
-//     at spørgsmålene vises i tilfældig rækkefølge.
-// Quizzerne skal kunne scores.Scoren skal anbringes i 
-// localstorage således, at en besvarer, skal kunne se 
-// sin score ved genbesøg på siden.
---
+++ js/quiz1.js
@@ -0,0 +1,30 @@
+export const q = {
+    name: 'test',
+    created: new Date(),
+    questions: [
+        {
+            category: 'continent',
+            question: 'on which continent is Denmark located?1',
+            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+            correctAnswer: 1
+        },
+        {
+            category: 'continent',
+            question: 'on which continent is Denmark located?2',
+            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+            correctAnswer: 3
+        },
+        {
+            category: 'continent',
+            question: 'on which continent is Denmark located?3',
+            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+            correctAnswer: 0
+        },
+        {
+            category: 'continent',
+            question: 'on which continent is Denmark located?4',
+            possibleAnswers: ['Europe', 'Asia', 'Africa', 'Antarctica'],
+            correctAnswer: 0
+        }
+    ]
+}
